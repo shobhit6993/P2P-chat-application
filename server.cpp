a937145e740a56e192418c90f2f5cee3e6b527e8
Shobhit Chaurasia <sc.shobhit@gmail.com>
2014-02-19 19:33:41 +0530

    server.cpp added

---
+++ server.cpp
@@ -0,0 +1,235 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <time.h>
+#include <map>
+#include <pthread.h>
+#include <mutex>
+#include <iostream>
+#include <csignal>
+using namespace std;
+
+#define PORT "8888"  // the port users will be connecting to
+
+#define BACKLOG 10	 // how many pending connections queue will hold
+
+#define MAXTHREADS 50
+
+#define TTL 5
+
+#define MAXDATASIZE 100
+
+struct info
+{
+	int sock_fd;
+	time_t ttl;
+	string ip;
+};
+
+std::map<string, info> clientMap;
+std::map<string, int> threadMap;
+pthread_t threads[MAXTHREADS];
+mutex mtx;
+
+//for recv timeout
+struct timeval tv;
+tv.tv_sec = 30;  /* 30 Secs Timeout */
+tv.tv_usec = 0;  // Not init'ing this can cause strange errors
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+
+void *threadForClient(void* s)
+{
+	string client_ip = (char*)s;
+	char* buf;
+	bool flag = true;
+	string table;
+
+	if((rv = recv(clientMap[client_ip].sock_fd, buf, 5, 0))>0)
+	{
+		if(strcmp (buf, "ping")==0)
+		{
+			//not even required as TTL is not the 30sec associated with recv timeout
+			//remove time entirely from map.
+			//if ping is recvd, do nothing.
+			mtx.lock();
+			clientMap[client_ip].ttl = time(0);
+			mtx.unlock();
+		}
+		if(strcmp (buf, "list")==0)
+		{
+			mtx.lock();
+			clientMap[client_ip].ttl = time(0);
+			for(auto it=clientMap.begin();it!=clientMap.end();it++)
+			{
+				cout << it->second.ip << " " << time(0)-it->second.ttl;
+				if(time(0) - it->second.ttl > TTL)
+				{
+					clientMap[it->second.ip].active = false;
+				}
+				else
+				{
+					table += it->second.ip;
+					table += "\n";
+				}
+			}
+			mtx.unlock();
+		}
+	}
+
+	if(rv<=0)		//recv timedout implies client no longer alive
+	{
+		mtx.lock();
+		close(clientMap[client_ip].sock_fd);
+		clientMap.erase(client_ip);
+		mtx.unlock();
+		pthread_exit(NULL);
+	}
+}
+
+
+int main(int argc, char const *argv[])
+{
+	int sockfd, new_fd, yes=1, rv, rc, threadCnt=0;  
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr; // connector's address information
+	socklen_t sin_size;
+	struct sigaction sa;
+
+	char s[INET6_ADDRSTRLEN];
+	string client_ip;
+
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; // use my IP
+
+	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and bind to the first we can
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("server: socket");
+			continue;
+		}
+
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+				sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+
+		//for recv timeout
+		if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv,sizeof(struct timeval)))
+		{
+			perror("setsockopt");
+			exit(1);
+		}
+
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("server: bind");
+			continue;
+		}
+
+		break;
+	}
+
+
+	if (p == NULL)  {
+		fprintf(stderr, "server: failed to bind\n");
+		return 2;
+	}
+
+	freeaddrinfo(servinfo); // all done with this structure
+
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	sa.sa_handler = sigchld_handler; // reap all dead processes
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+		perror("sigaction");
+		exit(1);
+	}
+
+	printf("server: waiting for connections...\n");
+
+	while(1)
+	{
+		sin_size = sizeof their_addr;
+		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		if (new_fd == -1) {
+			perror("accept");
+			continue;
+		}
+
+		inet_ntop(their_addr.ss_family,
+			get_in_addr((struct sockaddr *)&their_addr),
+			s, sizeof s);
+		printf("server: got connection from %s\n", s);
+
+		client_ip = s;
+
+		if(threadCnt<MAXTHREADS)
+		{
+			threadMap[client_ip]=threadCnt++;
+			if (send(new_fd, "CONN", 5, 0) == -1)	//send CONN msg to new client
+				perror("send");
+		}
+		else 
+		{
+			fprintf(stderr,"Server ran out of threads\n");
+			if (send(new_fd, "DISC", 5, 0) == -1)		//send DISC msg to new client
+				perror("send");
+			continue;
+		}
+		
+
+		mtx.lock();
+				
+		clientMap[client_ip].ip = client_ip;
+		clientMap[client_ip].sock_fd = new_fd;
+		clientMap[client_ip].ttl = time(0);
+
+		mtx.unlock();
+
+		if((rc = pthread_create(&threads[threadMap[client_ip]], NULL , threadForClient, (void*)s))!=0) ;
+	
+		if (rc){
+	     	fprintf(stderr,"Error:unable to create thread, %d\n",rc);
+	     	close(client_table[client_ip].sock_fd);
+	     	client_table.erase(client_ip);
+	  	}
+
+	}
+
+
+	return 0;
+}
